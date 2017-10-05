9f721aacc226936f0d2ee076083fa7d42e738967
Daniel <dannelinder21@gmail.com>
2017-10-05 20:17:03 +0200

    Generator created

--- SimplePasswordGenerator/Program.cs
+++ SimplePasswordGenerator/Program.cs
@@ -10,6 +10,27 @@ namespace SimplePasswordGenerator
     {
         static void Main(string[] args)
         {
+            // Length of the password 
+            int length = 8;
+
+            string passWord = generator(length);
+            Console.WriteLine(passWord);
+        }
+
+        private static string generator(int length)
+        {
+            // All characters used when generating password
+            const string chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            StringBuilder result = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+            
+            return result.ToString();
         }
     }
 }
